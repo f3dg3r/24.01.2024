75f6d6c3270c51276316e53356122fbca61bd915
Лебедев Георгий <f3dg3r@stud.vvsu.ru>
2024-01-31 17:52:26 +1000

    Point

---
+++ 31.01.2024.ts
@@ -0,0 +1,22 @@
+class ClassName {}
+class Point {x: number; y: number;}
+let vlad:Point = new Point();
+
+ function vlaad() {
+     vlad.x = 1;
+     vlad.y = 2;
+
+}
+
+vlaad()
+ 
+
+class Point1 {width: number; height:number;}
+let vlad1: Point1 = new Point1()
+
+function vlaad1() {
+    vlad1.width = 1;
+    vlad1.height = 1;
+}
+
+vlaad1()
