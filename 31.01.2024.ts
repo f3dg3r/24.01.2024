fc91188a5b8b6e07f7cc2a78d7e1730267b92774
Лебедев Георгий <f3dg3r@stud.vvsu.ru>
2024-02-07 18:02:00 +1000

    gg

--- 31.01.2024.ts
+++ 31.01.2024.ts
@@ -1,22 +1,22 @@
-class ClassName {}
-class Point {x: number; y: number;}
-let vlad:Point = new Point();
+class Rect{ 
+     private x1: number;
+     public x2: number; 
+     private readonly MAX_COORD = 1000;
+     y1: number; 
+     y2: number; 
+     constructor(x1?: number,x2?:number, y1?:number, y2?: number) {
+         this.x1
 
- function vlaad() {
-     vlad.x = 1;
-     vlad.y = 2;
-
+    }
+    square () {
+        return Math.abs( this.x1 - this.x2) *
+        Math.abs(this.y1 - this.y2)
+    }
 }
 
-vlaad()
- 
 
-class Point1 {width: number; height:number;}
-let vlad1: Point1 = new Point1()
+let rect1: Rect = new Rect(10,9) 
+let rect2: Rect = new Rect()
+rect1.x2 = 4
 
-function vlaad1() {
-    vlad1.width = 1;
-    vlad1.height = 1;
-}
-
-vlaad1()
+console.log(rect1.square())
