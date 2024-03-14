e67ecdef5b1a9c08dff0a3723e8b2e25c505f0ff
Лебедев Георгий <f3dg3r@stud.vvsu.ru>
2024-03-14 10:06:03 +1000

    qw

--- 31.01.2024.ts
+++ 31.01.2024.ts
@@ -39,25 +39,65 @@
 // temp2.setX = 6
 // console.log(temp2.getX)
 
-class Point {
-  x:number
-  y:number
-  constructor(val1?:number, val2?: number ){
-      this.x = val1
-      this.y = val2
-  }
-  }
+// class Point {
+//   x:number
+//   y:number
+//   constructor(val1?:number, val2?: number ){
+//       this.x = val1
+//       this.y = val2
+//   }
+//   }
   
-  class Triangle {
-      x: Point
-      y: Point
-      z: Point 
-      constructor(x1?: number, y1?: number, x2?: number, y2?: number, x3?: number, y3?: number){
+//   class Triangle {
+//       x: Point
+//       y: Point
+//       z: Point 
+//       constructor(x1?: number, y1?: number, x2?: number, y2?: number, x3?: number, y3?: number){
 
-      }
-    }
+//       }
+//     }
 
-    let point1: Point = new Point(5,6)
+//     let point1: Point = new Point(5,6)
 
-    let tr0: Triangle = new Triangle()
-    let tr1: Triangle = new Triangle(1,2,3,4,5,6)
+//     let tr0: Triangle = new Triangle()
+//     let tr1: Triangle = new Triangle(1,2,3,4,5,6)
+
+// class Point {
+//   static cnt:  number = 0;
+//   x: number
+//   y: number
+//   constructor(){
+//     this.x = 0
+//     this.y = 0
+//     Point.cnt++
+//   }
+//   public static getCounter():number {
+//     let x = 5
+//     return x;
+//   }
+// }
+
+// let point1: Point = new Point()
+// console.log(Point.cnt)
+// let point2: Point = new Point()
+
+// console.log(Point.cnt)
+
+
+// class ShopItem {
+//   public static id: number=0;
+//   public productName:string ;
+//   public size: number;
+//   public weight: number;
+//   public price: number;
+//   constructor() {
+//     this.productName = ''
+//     this.size =0
+//     this.weight=0
+//     this.price=0
+//     ShopItem.id++
+//   }
+// }
+
+
+
